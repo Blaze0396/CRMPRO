92d438e2d969a59672b702531e9c39e1ec41e49c
Blaze0396 <testing.hiten1996@gmail.com>
2024-08-13 13:18:25 +0530

    FrameWork is Ready

---
+++ ComCastCRMGUIFramework2Git/src/main/java/com/comcast/crm/objectRepositoryUtility/ContactPage.java
@@ -0,0 +1,29 @@
+package com.comcast.crm.objectRepositoryUtility;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+public class ContactPage {
+
+	@FindBy(xpath = "//img[@alt='Create Contact...']")
+	private WebElement createContactbtn;
+	
+	public ContactPage(WebDriver driver) {
+		PageFactory.initElements(driver, this);
+	}
+
+	public WebElement getCreateContactbtn() {
+		return createContactbtn;
+	}
+	
+	@FindBy(name="Search")
+	private WebElement ele1;
+	
+	@FindBy(name="Searchbtn2")
+	private WebElement ele2;
+	
+	@FindBy(xpath = "sss")
+	private WebElement ele3;
+}
