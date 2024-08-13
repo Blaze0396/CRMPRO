c7a2f3da9edbce908aec41a869e95432fe8de343
Blaze0396 <testing.hiten1996@gmail.com>
2024-08-13 14:47:21 +0530

    Product Created

---
+++ ComCastCRMGUIFramework2Git/src/test/java/com/comcast/crm/product/CreateProduct.java
@@ -0,0 +1,11 @@
+package com.comcast.crm.product;
+
+import org.testng.annotations.Test;
+
+public class CreateProduct {
+
+	@Test
+	public void createProduct() {
+		System.out.println("product created");
+	}
+}
